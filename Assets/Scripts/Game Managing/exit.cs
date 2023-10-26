5cd688f21a220cfc62ffa5d9775effda2efd95f3
rinko0515 <146147219+rinko0515@users.noreply.github.com>
2023-10-26 18:43:06 +1300

    exit

---
+++ Assets/Scripts/Game Managing/exit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class exit : MonoBehaviour
+{
+    public GameManager gameManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameManager.renderTotal >= 100)
+        {
+            
+        }
+    }
+}
