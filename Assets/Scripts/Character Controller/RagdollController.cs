6b036514a1755da518afe8b4adefd7b7e991d9c8
rinko0515 <146147219+rinko0515@users.noreply.github.com>
2023-10-27 15:10:16 +1300

    XD

--- Assets/Scripts/Character Controller/RagdollController.cs
+++ Assets/Scripts/Character Controller/RagdollController.cs
@@ -24,7 +24,7 @@ public class RagdollController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<PhotonView>().RPC("RagdollModeOff", RpcTarget.AllBuffered, null);
+        gameObject.GetComponent<PhotonView>().RPC("GetRagdollBits", RpcTarget.AllBuffered, null);
     }
 
     // Update is called once per frame
